4c820059696c7b3703df95fb4c7dfd0326e51000
stefanm334 <52058896+stefanm334@users.noreply.github.com>
2021-03-07 14:17:43 +0100

    upd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-import weatherData from '../data/weatherdata';
+const weatherData = {
+    tempUnit: 'C',
+    windSpeedUnit: 'm / s',
+    days: [
+        { day: 'Mon', temp: 22, windDirection: 'north - east', windSpeed: 10, type: 'sunny' },
+        { day: 'Tue', temp: 14, windDirection: 'north - west', windSpeed: 14, type: 'rainy' },
+        { day: 'Wed', temp: 17, windDirection: 'west', windSpeed: 20, type: 'cloudy' },
+        { day: 'Thu', temp: 14, windDirection: 'south - east', windSpeed: 22, type: 'cloudy' },
+        { day: 'Fri', temp: 27, windDirection: 'east', windSpeed: 20, type: 'windy' },
+        { day: 'Sat', temp: 10, windDirection: 'south', windSpeed: 15, type: 'cloudy' },
+        { day: 'Sun', temp: 13, windDirection: 'south - east', windSpeed: 13, type: 'cloudy' },
 
-console.log(weatherData)
+    ]
+}
+
+var dayList = $('.day-list')
+
+
+
+
+console.log(weatherData.days.length)
+for (let i = 0; i < weatherData.days.length; i++) {
+    var day = $('<div>').attr('class', 'day').attr('id', i)
+    var dayName = $('<div>').attr('class', 'day-name').text(weatherData.days[i].day)
+    var dayTemp = $('<div>').attr('class', 'day-temp').text(weatherData.days[i].temp)
+    var tempMeasuremnt = $('<div>').attr('class', 'measurment').text(weatherData.tempUnit)
+    day.append(dayName)
+    day.append(dayTemp)
+    day.append(tempMeasuremnt)
+
+    dayList.append()
+}
+
+
+var widget = $('.widget')
